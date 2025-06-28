3d5cde1122f8e2b228f870512502e8afb9c85899
SAHILJAIN2024 <jainsahil022@gmail.com>
2025-06-29 02:36:11 +0530

    read me added

--- CRX.sol
+++ CRX.sol
@@ -9,7 +9,7 @@ contract CarbonCreditToken is ERC20, Ownable {
         ERC20("Carbon Credit Token", "CRX")
         Ownable(initialOwner)
     {
-        _mint(initialOwner, 100 * 10 ** decimals()); // Mint exactly 100 CRX
+        _mint(initialOwner, 10 * 10 ** decimals()); 
     }
 
     function mint(address to, uint256 amount) external onlyOwner {
@@ -22,4 +22,4 @@ contract CarbonCreditToken is ERC20, Ownable {
 }
 
 # This smart contract has been deployed on remix ide 
-contract address - 0x0fed653E54835939f9f95f288Aecc742e029925f 
+contract address - 0xb3e497afCaB81fFb7690e3157D03715F0580B391
